9363c71611b5186898695c8bdc6ea30f5c512a5c
Russ Brasser <russ.brasser@gmail.com>
2015-08-06 12:25:50 -0400

    Add camera control script

---
+++ HackDay1/Assets/scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	public GameObject player;
+	private Vector3 offset;
+	private int zoom = 60; 
+	public float smooth = 5;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+		zoom = GetComponent<Camera>().fieldOfView;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+		if (Input.GetKeyDown ("z")) { 
+			zoom = Mathf.Max(10, zoom-10); 
+		} else if (Input.GetKeyDown ("x")) { 
+			zoom = Mathf.Min(100, zoom+10); 
+		}
+		
+		GetComponent<Camera>().fieldOfView = Mathf.Lerp (GetComponent<Camera>().fieldOfView, zoom, Time.deltaTime * smooth);
+	}
+}
