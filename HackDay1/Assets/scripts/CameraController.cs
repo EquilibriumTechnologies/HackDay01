c0fec7b5d6ebd2aac104625744a36f78db7c8bb3
Russ Brasser <russ.brasser@gmail.com>
2015-08-06 14:12:53 -0400

    Keep camera looking at the player

--- HackDay1/Assets/scripts/CameraController.cs
+++ HackDay1/Assets/scripts/CameraController.cs
@@ -6,22 +6,26 @@ public class CameraController : MonoBehaviour {
 	private Vector3 offset;
 	private float zoom = 60f;
 	public float smooth = 5f;
+	private Camera cam;
 
 	// Use this for initialization
 	void Start () {
+		cam = GetComponent<Camera> ();
 		offset = transform.position - player.transform.position;
-		zoom = GetComponent<Camera>().fieldOfView;
+		zoom = cam.fieldOfView;
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
 		transform.position = player.transform.position + offset;
+
 		if (Input.GetKeyDown ("z")) { 
 			zoom = Mathf.Max(10, zoom-10); 
 		} else if (Input.GetKeyDown ("x")) { 
 			zoom = Mathf.Min(100, zoom+10); 
 		}
 		
-		GetComponent<Camera>().fieldOfView = Mathf.Lerp (GetComponent<Camera>().fieldOfView, zoom, Time.deltaTime * smooth);
+		cam.fieldOfView = Mathf.Lerp (cam.fieldOfView, zoom, Time.deltaTime * smooth);
+		transform.LookAt (player.transform.position);
 	}
 }
