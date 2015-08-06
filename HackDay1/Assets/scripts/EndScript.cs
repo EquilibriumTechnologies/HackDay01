5186400abd3ee0404b615edb0333f6c15303018c
gbarton <garrett.barton@gmail.com>
2015-08-06 14:18:01 -0400

    added endgame button

---
+++ HackDay1/Assets/scripts/EndScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndScript : MonoBehaviour {
+
+	public GameObject winParticles;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision col) {
+		Debug.Log (col.gameObject.tag);
+		if (col.gameObject.tag == "Player") {
+			Debug.Log ("WIN");
+			Instantiate(winParticles,this.transform.position,this.transform.rotation);
+		}
+	}
+}
