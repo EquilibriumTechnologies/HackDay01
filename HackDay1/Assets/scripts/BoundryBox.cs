19264fd4ce5b5f100f20017c6229425cdc617bdf
gbarton <garrett.barton@gmail.com>
2015-08-06 14:53:10 -0400

    added death box

---
+++ HackDay1/Assets/scripts/BoundryBox.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoundryBox : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		Debug.Log ("DEAD");
+		Destroy(other.gameObject);
+	}
+}
